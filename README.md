9b35f759e7fb05924a570e26b782292c1b78ce27
yc.huang71 <yc.huang71@gmail.com>
2021-09-13 19:07:12 +0800

    feat: add pics

--- README.md
+++ README.md
@@ -1,6 +1,11 @@
 # my_ros2_transitions
 
-Combination of ROS2 and PyTransitions (Finit-State Machine).
+Combination of ROS2 and PyTransitions (Finit-State-Machine).
+
+|![](./pics/02.png)|
+|:-:|
+|![](./pics/01.png)|
+
 
 ## Requirements
 
@@ -9,3 +14,7 @@ transitions==0.8.9
 transitions-gui==0.1.0
 tornado==6.1
 ```
+
+## Notice
+
+The blocking execution of rclpy is the major problem.
--- my_ros2_transitions/machine.py
+++ my_ros2_transitions/machine.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-import threading
+from datetime import datetime
 
 from transitions import Machine, State
 from transitions_gui import WebMachine
@@ -15,30 +15,37 @@ class MyNode(Node):
     def __init__(self, fsm):
         """
         """
-        super().__init__(node_name="state_machine_node")
+        super().__init__(node_name="fsm_node")
         self.fsm = fsm
-        self.get_logger().info("state machine node starting ...")
+        self.get_logger().info("fsm node start")
         self.pub = self.create_publisher(msg_type=String, topic="demo_topic", qos_profile=10)
         self.create_service(srv_type=Empty, srv_name="/srv_goto_work", callback=self._cb_srv_goto_work)
         self.create_service(srv_type=Empty, srv_name="/srv_goto_idle", callback=self._cb_srv_goto_idle)
         self.create_service(srv_type=Empty, srv_name="/srv_print_state", callback=self._cb_srv_print_state)
-        self.create_service(srv_type=Empty, srv_name="/srv_force_idle", callback=self._cb_srv_force_idle)
 
     def _cb_srv_goto_work(self, req: Empty.Request, res: Empty.Response):
+        """
+        """
         self.get_logger().info("_cb_srv_goto_work")
         self.fsm.goto_work()
         return res
 
     def _cb_srv_goto_idle(self, req: Empty.Request, res: Empty.Response):
+        """
+        """
         self.get_logger().info("_cb_srv_goto_idle")
         self.fsm.goto_idle()
         return res
 
     def _cb_srv_print_state(self, req: Empty.Request, res: Empty.Response):
+        """
+        """
         self.get_logger().info("_cb_srv_print_state " + self.fsm.state)
         return res
 
     def _cb_srv_force_idle(self, req: Empty.Request, res: Empty.Response):
+        """
+        """
         self.get_logger().info("_cb_srv_force_idle")
         return res
 
@@ -56,41 +63,39 @@ class MyFSM(object):
     ]
 
     def __init__(self):
+        """
+        """
         self.node = MyNode(fsm=self)
         self.machine = WebMachine(model=self, states=__class__.states,
             transitions=__class__.transitions, initial="idle", name="My FSM")
+        self.state_work_timer = None
 
     def enter_idle(self):
-        msg = String()
-        msg.data = "enter state idle"
-        self.node.pub.publish(msg=msg)
-        self.node.get_logger().info(msg.data)
+        """
+        """
+        self.node.get_logger().info("enter state idle")
+        if self.state_work_timer:
+            self.state_work_timer.cancel()
 
     def enter_work(self):
-        msg = String()
-        msg.data = "enter state work"
-        self.node.pub.publish(msg=msg)
-        self.node.get_logger().info(msg.data)
+        """
+        """
+        self.node.get_logger().info("enter state work")
+        self.state_work_timer = self.node.create_timer(timer_period_sec=1.0, callback=self._cb_state_work_timer)
 
-        # rate = self.node.create_rate(1)
-        # while rclpy.ok():
-        #     rate.sleep()
-        #     if self.stop_state:
-        #         self.stop_state = False
-        #         break
-        #     else:
-        #         msg.data = "keep working"
-        #         self.node.pub.publish(msg=msg)
-        # self.go_to_idle()
+    def _cb_state_work_timer(self):
+        """
+        """
+        msg = String()
+        msg.data = datetime.now().strftime("%H:%M:%S")
+        self.last_job = str(msg.data)
+        self.node.pub.publish(msg=msg)
 
 
 def main():
     rclpy.init(args=None)
     fsm = MyFSM()
     rclpy.spin(node=fsm.node)
-    # thread = threading.Thread(target=rclpy.spin, args=(fsm.node, ), daemon=True)
-    # thread.start()
-    # thread.join()
     rclpy.shutdown()
 
 if __name__ == "__main__":
